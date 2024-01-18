5caec5f4a7ab7215b7cc717a8508701c0be86355
ruwan <ruwan.chamikara@dh3s.com>
2024-01-18 22:23:49 +0530

    added all changes

---
+++ src/test/java/com/rc/reactive/SpringReactiveMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
